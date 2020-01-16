bca18f85cb009e9d580635488d7c7e8f64c8b93a
Jamshid <jamshid.rasulov2000@gmail.com>
2020-01-16 17:23:29 +0500

    new api

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,6 @@ Route::get('courses/{id}', 'CourseController@show');
 Route::get('search/{searchTerm}', 'CourseController@search');
 Route::get('search/{searchType}/{type}', 'CourseController@searchtype');
 
-// Route::put('courses/{courses}', 'CoursesController@markAsCompleted');
-Route::post('properties', 'PropertyController@store');
-Route::get('properties', function(Request $request){
-    echo $request->url();
-});
 // Route::put('properties/{properties}', 'PropertiesController@markAsCompleted');
 /*
 |--------------------------------------------------------------------------
